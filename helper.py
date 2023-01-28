5510eee241675088f8b6210040c8717ad9d873ea
Meng Demao <mengdemao19951021@163.com>
2023-01-28 23:45:11 +0800

    更新opencv与python

--- helper.py
+++ helper.py
@@ -7,6 +7,7 @@ from rich import print
 
 # 设置显示图片
 import matplotlib.pyplot as plt
+import matplotlib
 
 # 导入numpy
 import numpy as np
@@ -21,7 +22,7 @@ import torchvision
 import torchvision.transforms as transforms
 
 # 导入sympy
-from sympy import * 
+from sympy import *
 
 # 导入延时模块
 import time
@@ -31,22 +32,109 @@ from IPython.display import display, Latex
 
 # 打开摄像头获取数据
 def getFrame():
-    cap = cv.VideoCapture(0)
-    ret,frame = cap.read()
-    cap.release()
-    return frame
+	cap = cv.VideoCapture(0)
+	ret,frame = cap.read()
+	cap.release()
+	return frame
 
 # 添加一个打印opencv的函数
-def displayImage(Image):
-    plt.imshow(Image)
-    plt.axis('off') # 关掉坐标轴为 off
-    plt.title('Catch Image') # 图像题目
-    plt.show()
-    
-def displayFrame(frame):
-    # 转换RGB顺序
-    frame=cv.cvtColor (frame, cv.COLOR_BGR2RGB)
-    plt.imshow(frame)
-    plt.axis('off') # 关掉坐标轴为 off
-    plt.title('Catch Image') # 图像题目
-    plt.show()
+def displayImage(Image, title):
+	plt.imshow(Image)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title) # 图像题目
+	plt.show()
+
+def displayFrame(frame, title):
+	# 转换RGB顺序
+	frame=cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+	plt.imshow(frame)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title) # 图像题目
+	plt.show()
+
+def display2Frame(frame, title):
+	# 转换RGB顺序
+	plt.subplot(121)
+	frame0 = cv.cvtColor(frame[0], cv.COLOR_BGR2RGB)
+	plt.imshow(frame0)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title[0]) # 图像题目
+
+	plt.subplot(122)
+	frame1 = cv.cvtColor(frame[1], cv.COLOR_BGR2RGB)
+	plt.imshow(frame1)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title[1]) # 图像题目
+	plt.show()
+
+
+def display4Frame(frame, title):
+	# 转换RGB顺序
+	plt.subplot(221)
+	frame0 = cv.cvtColor(frame[0], cv.COLOR_BGR2RGB)
+	plt.imshow(frame0)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title[0]) # 图像题目
+
+	plt.subplot(222)
+	frame1 = cv.cvtColor(frame[1], cv.COLOR_BGR2RGB)
+	plt.imshow(frame1)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title[1]) # 图像题目
+
+	plt.subplot(223)
+	frame2 = cv.cvtColor(frame[2], cv.COLOR_BGR2RGB)
+	plt.imshow(frame2)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title[2]) # 图像题目
+
+	plt.subplot(224)
+	frame3 = cv.cvtColor(frame[3], cv.COLOR_BGR2RGB)
+	plt.imshow(frame3)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title[3]) # 图像题目
+
+	plt.show()
+	return
+
+def display6Frame(frame, title):
+	# 转换RGB顺序
+	plt.subplot(231)
+	frame0 = cv.cvtColor(frame[0], cv.COLOR_BGR2RGB)
+	plt.imshow(frame0)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title[0]) # 图像题目
+
+	plt.subplot(232)
+	frame1 = cv.cvtColor(frame[1], cv.COLOR_BGR2RGB)
+	plt.imshow(frame1)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title[1]) # 图像题目
+
+	plt.subplot(233)
+	frame2 = cv.cvtColor(frame[2], cv.COLOR_BGR2RGB)
+	plt.imshow(frame2)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title[2]) # 图像题目
+
+	plt.subplot(234)
+	frame3 = cv.cvtColor(frame[3], cv.COLOR_BGR2RGB)
+	plt.imshow(frame3)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title[3]) # 图像题目
+
+	plt.subplot(235)
+	frame4 = cv.cvtColor(frame[4], cv.COLOR_BGR2RGB)
+	plt.imshow(frame4)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title[4]) # 图像题目
+
+	plt.subplot(236)
+	frame5 = cv.cvtColor(frame[5], cv.COLOR_BGR2RGB)
+	plt.imshow(frame5)
+	plt.axis('off') # 关掉坐标轴为 off
+	plt.title(title[5]) # 图像题目
+	plt.xticks([])
+	plt.yticks([])
+	plt.show()
+	return
