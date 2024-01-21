4ed339ec00cc6591961a09b2567af0a49ff1a8a8
Meng Demao <mengdemao19951021@163.com>
2024-01-21 20:30:34 +0800

    更新依赖文件

--- helper.py
+++ helper.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-# 修改打印函数
-# 覆盖系统
-from rich import print
-
 # 设置显示图片
 import matplotlib.pyplot as plt
 import matplotlib
@@ -17,12 +13,6 @@ import numpy as np
 
 # 导入opencv
 import cv2 as cv
-
-# 导入pytorch
-import torch
-from torch import autograd
-import torchvision
-import torchvision.transforms as transforms
 
 # 导入sympy
 from sympy import *
